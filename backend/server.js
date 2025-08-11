3cd3173ab5c04a4362be79c919bb59497a19cf0d
Ayush <singhayush.sa81@gmail.com>
2025-08-11 13:39:06 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
 const app = express();
 const routes = require("./routes/index");
 
@@ -11,18 +12,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.LOCAL_VITE_URL,
+  process.env.LOCAL_REACT_URL,
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 // Routes
 app.use("/api", routes);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Not Found' });
+  res.status(404).json({ message: "Not Found" });
 });
-
 
 connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
